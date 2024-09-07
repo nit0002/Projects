bf6cc7f82930c329319855296ce0ee17052a44aa
nit0002 <nitinsanodiya88@gmail.com>
2024-09-08 00:07:33 +0530

    This is calculator  project index.html file

---
+++ scri[t.js
@@ -0,0 +1,26 @@
+var input = document.getElementById('inputbox');
+var buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'Ac'){
+            string = "0";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'Del'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+
+        }
+    })
+})
